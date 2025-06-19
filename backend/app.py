ee339af0fc9bbf5e426c66c53b9802173b7f67ce
BCassO <cassorionbw@gmail.com>
2025-06-19 17:07:48 +0530

    Fix Werkzeug production error

--- backend/app.py
+++ backend/app.py
@@ -187,4 +187,4 @@ def handle_leave_room(data):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_ENV') != 'production'
-    socketio.run(app, host='0.0.0.0', port=port, debug=debug)
+    socketio.run(app, host='0.0.0.0', port=port, debug=debug, allow_unsafe_werkzeug=True)
